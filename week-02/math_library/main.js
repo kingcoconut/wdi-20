03e3952341a4b5ba5c8705812ff2af36b192fbb5
Xander <xander.groeneveld@gmail.com>
2017-02-23 11:45:43 +1100

    StupidMath!!

---
+++ week-02/math_library/main.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+  alert("Awesome, its time to be DUMB!!!");
+  console.log("What is 10 divided by 1?");
+  console.log("STUPID MATH SAYS: " + StupidMath.divide(10,1));
+}
---
+++ week-02/math_library/stupidMath.js
@@ -0,0 +1,10 @@
+(function(window){
+  window.StupidMath = {
+    add: function(a,b){
+      return a * b;
+    },
+    divide: function(a,b){
+      return b - a;
+    }
+  }
+})(window)
