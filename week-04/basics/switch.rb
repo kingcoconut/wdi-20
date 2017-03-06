9509740a863680f0005f32f731ad11d1941e2f79
Xander <xander.groeneveld@gmail.com>
2017-03-06 11:54:33 +1100

    day 1 files

---
+++ week-04/basics/switch.rb
@@ -0,0 +1,16 @@
+puts "What is your name?"
+
+person = gets.chomp
+
+case person
+when "Ben"
+  puts "Hey there!"
+when "John"
+  puts "Get lost!"
+when "Lucy"
+  puts "Hope you have a delightful day"
+when "Sally"
+  puts "Why are you here?"
+else
+  puts "Get lost, you weren't invited!"
+end
