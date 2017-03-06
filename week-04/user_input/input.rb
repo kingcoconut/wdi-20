43927a94b1cd7b0097b691a6cccd28a8b5d34dbe
Xander <xander.groeneveld@gmail.com>
2017-03-06 11:19:04 +1100

    Ruby inputs

---
+++ week-04/user_input/input.rb
@@ -0,0 +1,9 @@
+puts "What is your first name?"
+
+first_name = gets.chomp
+
+puts "And what is your favourite coffee shop?"
+
+coffee_shop = gets.chomp
+
+puts "Your name is #{first_name} and your favourite coffee shop is #{coffee_shop}"
