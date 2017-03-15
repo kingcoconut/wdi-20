1c8ff3d509c3be3e2b9673ddd33e7d511290e8e1
Xander <xander.groeneveld@gmail.com>
2017-03-15 15:28:39 +1100

    base interface functionality complete

--- week-05/trangr/config/routes.rb
+++ week-05/trangr/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "countries#index"
+  get "/countries", to: "countries#show"
+
+  resources :cities, only: [:show]
+  resources :activities, only: [:show]
 end
