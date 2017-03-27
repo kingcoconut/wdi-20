a2bc7ebc497f3683b4c3c7c65c8b02816260a432
Xander <xander.groeneveld@gmail.com>
2017-03-27 14:39:06 +1100

    more ajax

--- week-07/ajax/main.js
+++ week-07/ajax/main.js
@@ -1,19 +1,25 @@
-window.onload = function(){
-  document.getElementById('search').addEventListener("click", function(){
+$(document).ready(function(){
+  $("#search").on("click", function(){
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
       if(xhr.readyState == 4){
         // we must have finished downloading the response :)
         var json_response = JSON.parse(xhr.responseText);
         json_response.Search.forEach(function(el){
-          var heading = document.createElement("h2");
-          title = document.createTextNode(el.Title);
-          heading.appendChild(title);
-          document.body.append(heading);
+          $("body").append($("<h2>").html(el.Title));
+          var img = $("<img>").attr("src", el.Poster).attr("movie-id", el.imdbID);
+          $("body").append(img);
         });
       }
     };
     xhr.open("GET", "http://www.omdbapi.com/?s=a&y=2016&type=movie");
     xhr.send();
   })
-};
+
+  console.log("Setup img event handling");
+
+  $(document).on("click", "img", function(){
+    var id = $(this).attr("movie-id");
+    console.log("Have fun here :)");
+  });
+});
