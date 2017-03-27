fbc04a7db0f460c74e3085e47c386eadc7917357
Xander <xander.groeneveld@gmail.com>
2017-03-27 12:56:15 +1100

    Ajax getting fancy

--- week-07/ajax/main.js
+++ week-07/ajax/main.js
@@ -1,13 +1,19 @@
 window.onload = function(){
-  var xhr = new XMLHttpRequest();
-  console.log(xhr.readyState)
-  xhr.onreadystatechange = function(){
-    if(xhr.readyState == 4){
-      // we must have finished downloading the response :)
-      alert("Download finished");
-      console.log(xhr.responseText);
-    }
-  };
-  xhr.open("GET", "http://www.omdbapi.com/?s=a&y=2016&type=movie");
-  xhr.send();
+  document.getElementById('search').addEventListener("click", function(){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState == 4){
+        // we must have finished downloading the response :)
+        var json_response = JSON.parse(xhr.responseText);
+        json_response.Search.forEach(function(el){
+          var heading = document.createElement("h2");
+          title = document.createTextNode(el.Title);
+          heading.appendChild(title);
+          document.body.append(heading);
+        });
+      }
+    };
+    xhr.open("GET", "http://www.omdbapi.com/?s=a&y=2016&type=movie");
+    xhr.send();
+  })
 };
