a0f032b629a6855e48bf2d34a2ea2899f5c3278b
Xander <xander.groeneveld@gmail.com>
2017-03-31 15:03:40 +1100

    Incredibly fantastic updates

--- week-07/postsApp/js/main.js
+++ week-07/postsApp/js/main.js
@@ -15,41 +15,68 @@ var PostView = Backbone.View.extend({
   }
 });
 
-var AppView = Backbone.View.extend({
+var AllPostsView = Backbone.View.extend({
   el: "#main",
   render: function(){
     var $header = $("<h1>").text("WELCOME")
     this.$el.html($header)
-    var self = this;
+    var mainElement = this.$el;
     this.collection.each(function(post){
       var postView = new PostView({model: post});
       postView.render();
-      self.$el.append(postView.el);
+      mainElement.append(postView.el);
     })
   }
 });
 
-$(document).ready(function(){
-  var post = new Post({
-    id: 1,
-    title: "Interesting Developments in Geopolitics",
-    content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi"
-  });
+var SinglePostView = Backbone.View.extend({
+  el: "#main",
+  render: function(){
+    var postView = new PostView({model: this.model});
+    postView.render();
+    this.$el.html(postView.el);
+  }
+});
 
-  var postTwo = new Post({
-    id: 2,
-    title: "PART II: Interesting Developments in Geopolitics",
-    content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi"
-  });
-
-  var postThree = new Post({
-    id: 3,
-    title: "PART III: Interesting Developments in Geopolitics",
-    content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi"
-  });
-
-  var blogPosts = new Posts([post, postTwo, postThree]);
-
-  var appView = new AppView({collection: blogPosts});
-  appView.render();
+var AppRouter = Backbone.Router.extend({
+  routes: {
+    "": "index",
+    "posts/:id": "showPost"
+  },
+  index: function(){
+    var appView = new AllPostsView({collection: App.blogPosts});
+    appView.render();
+  },
+  showPost: function(id){
+    var post = App.blogPosts.get(id);
+    var view = new SinglePostView({model: post});
+    view.render();
+  }
 })
+
+var App = {}
+
+var post = new Post({
+  id: 1,
+  title: "Interesting Developments in Geopolitics",
+  content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi"
+});
+
+var postTwo = new Post({
+  id: 2,
+  title: "PART II: Interesting Developments in Geopolitics",
+  content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi"
+});
+
+var postThree = new Post({
+  id: 3,
+  title: "PART III: Interesting Developments in Geopolitics",
+  content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi"
+});
+
+App.blogPosts = new Posts([post, postTwo, postThree]);
+App.router = new AppRouter();
+
+$(document).ready(function(){
+  Backbone.history.start("/");
+});
