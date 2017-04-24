6568c9a978d296edb06ef0c7b255565844cbdf49
Xander <xander.groeneveld@gmail.com>
2017-04-24 16:14:24 +1000

    Babel setup

---
+++ week-11/babel/main.bundle.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var foo = 123;
+
+var arr = [1, 2, 3, 4];
+
+arr.map(function (el) {
+  return el * 2;
+});
---
+++ week-11/babel/main.js
@@ -0,0 +1,5 @@
+let foo = 123;
+
+const arr = [1,2,3,4]
+
+arr.map((el) => el * 2)
