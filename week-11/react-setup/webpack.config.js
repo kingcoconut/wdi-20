a6924ffe7a3106adb4f9d3738a936f0516802679
Xander <xander.groeneveld@gmail.com>
2017-04-26 12:11:58 +1000

    basic setup for webpack with react

---
+++ week-11/react-setup/webpack.config.js
@@ -0,0 +1,28 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + "/app/index.html",
+  filename: "index.html",
+  inject: 'body'
+})
+
+module.exports = {
+  entry: [
+    __dirname + "/app/index.js"
+  ],
+  output: {
+    path: __dirname + "/dist",
+    filename: "index_bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js?$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    HTMLWebpackPluginConfig
+  ]
+}
